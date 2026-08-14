47619033f14068fa015687017536da838faf4269
billy4479 <giachi.ellero@gmail.com>
2026-08-14 12:29:04 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            gopls
+            golangci-lint
+            air
+          ];
+
+          nativeBuildInputs = with pkgs; [ go ];
+        };
+
+        packages = rec {
+          app = pkgs.callPackage ./package.nix { };
+          default = app;
+        };
+      }
+    );
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/billy4479/agent-up
+
+go 1.26.5
